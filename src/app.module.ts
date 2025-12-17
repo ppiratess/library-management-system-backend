e7866f8544d3e87d34b9fba46b3be52ed0578a5b
Shubham <tamrakarshubham23@gmail.com>
2025-12-17 10:55:30 +0000

    feat: create modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
+import { RentalsModule } from './rentals/rentals.module';
 
 @Module({
-  imports: [],
+  imports: [UsersModule, BooksModule, RentalsModule],
   controllers: [AppController],
   providers: [AppService],
 })
