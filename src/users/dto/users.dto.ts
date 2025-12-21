4bda121562cfbc4f075271fd859efb9ef3961c77
Shubham <tamrakarshubham23@gmail.com>
2025-12-21 14:56:52 +0000

    fix: refactor get all users dto

--- src/users/dto/users.dto.ts
+++ src/users/dto/users.dto.ts
@@ -1,14 +1,12 @@
 import {
   IsEmail,
   IsEnum,
-  IsInt,
   IsOptional,
   IsString,
-  Min,
   MinLength,
 } from 'class-validator';
-import { Type } from 'class-transformer';
 
+import { ListDto } from 'src/common/dto';
 import { Role } from 'src/generated/prisma/enums';
 
 export class CreateUserDto {
@@ -28,23 +26,7 @@ export class CreateUserDto {
   password: string;
 }
 
-export class GetUsersQueryDto {
-  @IsOptional()
-  @Type(() => Number)
-  @IsInt()
-  @Min(1)
-  page?: number = 1;
-
-  @IsOptional()
-  @Type(() => Number)
-  @IsInt()
-  @Min(1)
-  perPage?: number = 10;
-
-  @IsOptional()
-  @IsString()
-  search?: string;
-
+export class GetUsersQueryDto extends ListDto {
   @IsOptional()
   @IsEnum(Role)
   role?: Role;
