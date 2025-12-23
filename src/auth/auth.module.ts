21cd424228144d3e52e1e0df55423604944a76eb
Shubham <tamrakarshubham23@gmail.com>
2025-12-23 17:24:27 +0000

    feat: login and generate jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,20 @@
+import { JwtModule } from '@nestjs/jwt';
+
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { UsersModule } from 'src/users/users.module';
+import { UsersService } from 'src/users/users.service';
 
 @Module({
-  providers: [AuthService],
-  controllers: [AuthController]
+  imports: [
+    UsersModule,
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+      signOptions: { expiresIn: '1d' },
+    }),
+  ],
+  providers: [AuthService, UsersService],
+  controllers: [AuthController],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,41 @@
+import { JwtService } from '@nestjs/jwt';
 import { Injectable } from '@nestjs/common';
+import { User } from 'src/generated/prisma/client';
+
+import { LoginDto } from './dto/login.dto';
+import { UsersService } from 'src/users/users.service';
+import { comparePassword } from 'src/util/password.util';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(body: LoginDto) {
+    const { email, password } = body;
+
+    const user = await this.usersService.findByEmail(email);
+
+    if (!user) return null;
+
+    const isValid = await comparePassword(password, user.password);
+
+    if (!isValid) return null;
+
+    return user;
+  }
+
+  login(user: User) {
+    const payload = {
+      sub: user.id,
+      role: user.role,
+    };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+      role: user.role,
+    };
+  }
+}
